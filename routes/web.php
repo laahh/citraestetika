f225f54219e17bb5bb26f64b19e2ef3b4f08f2df
IFA APRILLIANTO <ifaaprilianto@student.telkomuniversity.ac.id>
2025-03-29 11:18:00 +0700

    dashboard components

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,18 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\landingpageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[landingpageController::class,'index']);
+Route::get('/dashboard',[DashboardController::class,'index']);
 Route::view('/home', 'landingpage.home');
 Route::view('/about', 'landingpage.about');
 Route::view('/product', 'landingpage.product');
 Route::view('/blog', 'landingpage.blog');
 
 
+// Route::prefix('dashboard')->group(function () {
+//     Route::get('/', [DashboardController::class, 'index']);
+// });
 
